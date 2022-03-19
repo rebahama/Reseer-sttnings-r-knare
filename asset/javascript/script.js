8184908b5906978887a06f317eac1f0328dac36b
rebahama <wiri@hotmail.se>
2022-03-19 18:49:56 +0000

    add adr statemen to function

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,28 +1,27 @@
-let passDistans=document.getElementById('job-distans');
-let countBtn=document.getElementById('count-btn');
-let passShift=document.getElementById('pass-shift');
-let result=document.getElementById('result');
-let resultTwo=document.getElementById('result-2');
+let passDistans = document.getElementById('job-distans');
+let countBtn = document.getElementById('count-btn');
+let passShift = document.getElementById('pass-shift');
+let result = document.getElementById('result');
+let resultTwo = document.getElementById('result-2');
 
 
 countBtn.addEventListener('click', countAll)
 
-function countAll(){
-let pass=parseFloat(passShift.value);
-let distans=parseFloat(passDistans.value);
-let summanm=pass*distans*18.50;
-let everyThing = pass*distans*18.50-11000
-if(everyThing<0)
-{
-    result.innerHTML=('Du har tyvärr ingen rätt till ersättning')
-    result.style.display="block";
-    resultTwo.style.display="none";
+function countAll() {
+    let pass = parseFloat(passShift.value);
+    let distans = parseFloat(passDistans.value);
+    let summanm = pass * distans * 18.50;
+    let everyThing = pass * distans * 18.50 - 11000
+    let newPass =parseInt(passShift.value)
+    if (everyThing < 0||newPass>365) {
+        result.innerHTML = ('Du har tyvärr ingen rätt till ersättning')
+        result.style.display = "block";
+        resultTwo.style.display = "none";
+    } else if (everyThing > 0) {
+        result.innerHTML = `Du kommer att få tillbaka ${Math.round(everyThing*0.30)} Kr i reseersättning`;
+        result.style.display = "block";
+        resultTwo.innerHTML = `Du skriver summan ${Math.round(summanm)} Kr i deklarationen.`;
+        resultTwo.style.display = "block";
+    }
+   console.log(newPass)
 }
-else if(everyThing>0){
-result.innerHTML=`Du kommer att få tillbaka ${Math.round(everyThing*0.30)} Kr i reseersättning`;
-result.style.display="block";
-resultTwo.innerHTML=`Du skriver summan ${Math.round(summanm)} Kr i deklarationen.`;
-resultTwo.style.display="block";
-}
-}
-
