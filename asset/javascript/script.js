1f02098a6372a8aaf6de471df1c0a630b3bba1ee
rebahama <wiri@hotmail.se>
2022-03-19 18:15:31 +0000

    add if statements for required amount

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -12,11 +12,17 @@ let pass=parseFloat(passShift.value);
 let distans=parseFloat(passDistans.value);
 let summanm=pass*distans*18.50;
 let everyThing = pass*distans*18.50-11000
-
+if(everyThing<0)
+{
+    result.innerHTML=('Du har tyvärr ingen rätt till ersättning')
+    result.style.display="block";
+    resultTwo.style.display="none";
+}
+else if(everyThing>0){
 result.innerHTML=`Du kommer att få tillbaka ${Math.round(everyThing*0.30)} Kr i reseersättning`;
 result.style.display="block";
 resultTwo.innerHTML=`Du skriver summan ${Math.round(summanm)} Kr i deklarationen.`;
 resultTwo.style.display="block";
-
+}
 }
 
