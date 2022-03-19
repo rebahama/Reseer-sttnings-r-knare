9d2a7bf94140550f376cea76a286d98e7fff8920
rebahama <wiri@hotmail.se>
2022-03-19 14:59:00 +0000

    style for mobile screen

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -2,6 +2,8 @@ let passDistans=document.getElementById('job-distans');
 let countBtn=document.getElementById('count-btn');
 let passShift=document.getElementById('pass-shift');
 let result=document.getElementById('result');
+let resultTwo=document.getElementById('result-2');
+
 
 countBtn.addEventListener('click', countAll)
 
@@ -11,9 +13,10 @@ let distans=parseInt(passDistans.value);
 
 let everyThing = pass*distans*18.50;
 
-result.innerHTML=`Du kommer att få tillbaka ${everyThing*0.30} Kr`;
+result.innerHTML=`Du kommer att få tillbaka ${everyThing-11000} Kr i reseersättning`;
 result.style.display="block";
-
+resultTwo.innerHTML=`Du skriver summan ${everyThing} Kr i deklarationen.`;
+resultTwo.style.display="block";
 
 }
 
